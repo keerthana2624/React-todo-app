25fce7f9339ae4dc323decd608b150548301fc45
keerthana2624 <keerthanamummy2002@gmail.com>
2024-12-11 15:34:09 +0530

    TaskList.css component is created

--- to-do-app/src/App.js
+++ to-do-app/src/App.js
@@ -1,2 +1,26 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom"; // Correct import
+import TaskList from "./components/TaskList";
+
+const App = () => {
+  return (
+    <Router>
+      <div className="app-container">
+        <header>
+          <h1>To-Do List App</h1>
+          {/* Optional Navigation Links */}
+        </header>
+        <main>
+          <Routes>
+            {/* Route for the task list */}
+            <Route path="/task-list" element={<TaskList />} />
+            {/* Redirect to task list */}
+            <Route path="/" element={<Navigate to="/task-list" />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
