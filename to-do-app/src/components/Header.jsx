9739de9370de33a309939247456ecb5b9407584d
keerthana2624 <keerthanamummy2002@gmail.com>
2024-12-12 20:56:48 +0530

    imported the css in the header component

--- to-do-app/src/components/Header.jsx
+++ to-do-app/src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-// import "./Header.css";
+import "./Header.css";
 import DarkModeToggle from "./DarkModeToggle"; 
 const Header = ({ toggleDarkMode, isDarkMode }) => {
   return (
