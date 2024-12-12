17f395378964dc15fe7f31523d1c2defe3841ffe
keerthana2624 <keerthanamummy2002@gmail.com>
2024-12-12 23:39:36 +0530

    styling modified to search filters

--- to-do-app/src/App.js
+++ to-do-app/src/App.js
@@ -1,63 +1,12 @@
 
 
-
-
-// import React from "react";
-// import { BrowserRouter as Router, Route, Routes, Navigate, NavLink } from "react-router-dom";
-// import TaskList from "./components/TaskList";
-// import AddTask from "./components/AddTask";
-// import "./App.css"; // For the navigation styles
-
-// const App = () => {
-//   return (
-//     <Router>
-//       <div className="app-container">
-//         <header>
-//           <h1>To-Do List App</h1>
-//           {/* Navigation Links */}
-//           <nav>
-//             <ul>
-//               <li>
-//                 <NavLink 
-//                   to="/task-list" 
-//                   className={({ isActive }) => (isActive ? "active-link" : "inactive-link")}
-//                 >
-//                   Task List
-//                 </NavLink>
-//               </li>
-//               <li>
-//                 <NavLink 
-//                   to="/add-task" 
-//                   className={({ isActive }) => (isActive ? "active-link" : "inactive-link")}
-//                 >
-//                   Add Task
-//                 </NavLink>
-//               </li>
-//             </ul>
-//           </nav>
-//         </header>
-//         <main>
-//           <Routes>
-//             <Route path="/add-task" element={<AddTask />} />
-//             <Route path="/task-list" element={<TaskList />} />
-//             <Route path="/" element={<Navigate to="/task-list" />} />
-//           </Routes>
-//         </main>
-//       </div>
-//     </Router>
-//   );
-// };
-
-// export default App;
-
-
-
 import React from "react";
 import { BrowserRouter as Router, Route, Routes, Navigate, NavLink } from "react-router-dom";
 import TaskList from "./components/TaskList";
 import AddTask from "./components/AddTask";
-import "./App.css"; // For the navigation styles
+import "./App.css"; 
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
@@ -101,6 +50,7 @@ const App = () => {
             <Route path="/" element={<Navigate to="/task-list" />} />
           </Routes>
         </main>
+        <Footer /> 
       </div>
     </Router>
   );
